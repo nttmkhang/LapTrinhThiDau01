f45141df68086635dc20c781b45410130c8de6b6
Binkeurss <nvbinh271104@gmail.com>
2023-07-03 14:56:49 +0700

    Bink

--- 3.3.3_CPP/3.3.3_CPP.cpp
+++ 3.3.3_CPP/3.3.3_CPP.cpp
@@ -1,20 +1,29 @@
-// 3.3.3_CPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
+long long countDivisors_3(long long);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+long long countDivisors_3(long long n)
+{
+    long long T = 1;
+    long long p = 2;
+    while (n > 1 && p * p <= n)
+    {
+        long long count = 0;
+        while (n % p == 0)
+        {
+            count++;
+            n /= p;
+        }
+        T = T * (count + 1);
+        p++;
+    }
+    if (n > 1)
+        T = T * 2;
+    return T;
+}
