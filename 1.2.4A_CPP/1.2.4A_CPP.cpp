44ee4f959541bd91d84c3a95cbbf89ee8ca5cb33
elodisme1 <22520254@gm.uit.edu.vn>
2023-07-03 15:11:22 +0700

    Do add new project

---
+++ 1.2.4A_CPP/1.2.4A_CPP.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+char toChar(int);
+string DecimalToBinary(long long);
+
+int main()
+{
+	long long n;
+	cout << "Nhap n (n <= 10^9): ";
+	cin >> n;
+
+	cout << "\nBieu dien so thap phan ";
+	cout << n << " o he co so 2 la: ";
+	cout << DecimalToBinary(n);
+
+	cout << "\n\n\nKet thuc!!!!";
+	return 1;
+}
+
+char toChar(int n)
+{
+	char Letter[] = { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
+	return Letter[n];
+}
+
+string DecimalToBinary(long long n) {
+	string temp;
+	while (n > 0)
+	{
+		if (n % 2 != 0)
+			temp = '1' + temp;
+		else
+			temp = '0' + temp;
+		n = n / 2;
+	}
+	return temp;
+}
