c3d38ff1a7975ec13f90fe366c4ced7d6d6e516f
elodisme1 <22520254@gm.uit.edu.vn>
2023-07-03 23:19:19 +0700

    Do add new project

--- 2.3.2C_Python/_2.3.2C_Python.py
+++ 2.3.2C_Python/_2.3.2C_Python.py
@@ -1,1 +1,16 @@
+def ktNguyenTo(n):
+    for i in range (2, n//2):
+        if n % i == 0:
+            return 0
+    return n > 1
+
+def main():
+    n = int(input("Nhap n: "))
+    if ktNguyenTo(n) == True:
+        print("n la so nguyen to")
+    else:
+        print("n khong la so nguyen to")
+
+if __name__ == "__main__":
+    main()
 
