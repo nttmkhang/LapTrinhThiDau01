03018e2a32c41a20b249da77a1234c63931d21de
elodisme1 <22520254@gm.uit.edu.vn>
2023-07-03 15:12:01 +0700

    Do add new project

--- 1.2.4A_Python/_1.2.4A_Python.py
+++ 1.2.4A_Python/_1.2.4A_Python.py
@@ -1,1 +1,19 @@
+def DecimalToBinary(n):
+    temp = ""
+    while n > 0:
+        if n % 2 != 0:
+            temp = '1' + temp
+        else:
+            temp = '0' + temp
+        n //= 2
+    return temp
 
+def main():
+    n = int(input("Nhap n (n <= 10^9): "))
+    print("Bieu dien so thap phan ")
+    print(n, "o he co so 2 la: ")
+    print(DecimalToBinary(n))
+    print("\n\n\nKet thuc!!!!")
+
+if __name__ == "__main__":
+    main()
