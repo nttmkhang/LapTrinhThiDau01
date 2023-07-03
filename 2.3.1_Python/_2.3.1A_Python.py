942aa9e265d50cc2f56fdac1e8a9c84991c77bcf
zFumei <huongzangngo@gmail.com>
2023-07-03 14:16:55 +0700

    Giang

--- 2.3.1_Python/_2.3.1A_Python.py
+++ 2.3.1_Python/_2.3.1A_Python.py
@@ -6,10 +6,3 @@ def ktNguyenTo(n):
     if dem == 2:
         return True
     return False
-
-def ktNguyenTo(n):
-    dem = 0
-    for i in range (1, n + 1):
-        if n % i == 0:
-            dem += 1
-    return dem == 2
