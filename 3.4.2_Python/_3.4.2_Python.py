83fe38d1bca91483da234c554614b1753f4cb417
elodisme1 <22520254@gm.uit.edu.vn>
2023-07-03 13:40:11 +0700

    Do add new project

--- 3.4.2_Python/_3.4.2_Python.py
+++ 3.4.2_Python/_3.4.2_Python.py
@@ -1,1 +1,13 @@
-
+def sumDivisors_2(n):
+    T = 1
+    p = 2
+    while n > 1:
+        count = 0
+        power = 1
+        while n % p == 0:
+            power *= p
+            count += 1
+            n /= p
+        T = T * (power * p - 1) / (p - 1)
+        p += 1
+    return T
